1216114d14ec12078d7cd4be0ac2a0ce48256430
monguri <mongry@gmail.com>
2018-05-03 16:07:36 +0900

    AnimNode_LookAtPracticeのいくつかの関数のひながたを作った

--- Plugins/IKPluginSandbox/Source/IKPluginSandbox/Private/AnimNode_LookAtPractice.cpp
+++ Plugins/IKPluginSandbox/Source/IKPluginSandbox/Private/AnimNode_LookAtPractice.cpp
@@ -1,7 +1,38 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "AnimNode_LookAtPractice.h"
+#include "BoneContainer.h"
 
 
+FAnimNode_LookAtPractice::FAnimNode_LookAtPractice()
+	: LookAtLocation(FVector(100.f, 0.f, 0.f))
+{
+}
 
+void FAnimNode_LookAtPractice::UpdateInternal(const FAnimationUpdateContext& Context)
+{
+}
+
+void FAnimNode_LookAtPractice::Initialize_AnyThread(const FAnimationInitializeContext& Context)
+{
+}
+
+void FAnimNode_LookAtPractice::EvaluateComponentSpaceInternal(FComponentSpacePoseContext& Context)
+{
+}
+
+void FAnimNode_LookAtPractice::EvaluateSkeletalControl_AnyThread(FComponentSpacePoseContext& Output, TArray<FBoneTransform>& OutBoneTransforms)
+{
+}
+
+bool FAnimNode_LookAtPractice::IsValidToEvaluate(const USkeleton* Skeleton, const FBoneContainer& RequiredBones)
+{
+	return true;
+}
+
+void FAnimNode_LookAtPractice::InitializeBoneReferences(const FBoneContainer& RequiredBones)
+{
+	BoneToModify.Initialize(RequiredBones);
+	LookAtTarget.InitializeBoneReferences(RequiredBones);
+}
 
--- Plugins/IKPluginSandbox/Source/IKPluginSandbox/Public/AnimNode_LookAtPractice.h
+++ Plugins/IKPluginSandbox/Source/IKPluginSandbox/Public/AnimNode_LookAtPractice.h
@@ -19,6 +19,30 @@ struct IKPLUGINSANDBOX_API FAnimNode_LookAtPractice : public FAnimNode_SkeletalC
 	UPROPERTY(EditAnywhere, Category=SkeletalControl) 
 	FBoneReference BoneToModify;
 	
-	
-	
+	/** Target socket to look at. Used if LookAtBone is empty. - You can use  LookAtLocation if you need offset from this point. That location will be used in their local space. **/
+	UPROPERTY(EditAnywhere, Category = Target)
+	FBoneSocketTarget LookAtTarget;
+
+	/** Target Offset. It's in world space if LookAtBone is empty or it is based on LookAtBone or LookAtSocket in their local space*/
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Target, meta = (PinHiddenByDefault))
+	FVector LookAtLocation;
+
+	// in the future, it would be nice to have more options, -i.e. lag, interpolation speed
+	FAnimNode_LookAtPractice();
+
+	// FAnimNode_Base interface
+	virtual void UpdateInternal(const FAnimationUpdateContext& Context) override;
+	virtual void Initialize_AnyThread(const FAnimationInitializeContext& Context) override;
+	// End of FAnimNode_Base interface
+
+	// FAnimNode_SkeletalControlBase interface
+	virtual void EvaluateComponentSpaceInternal(FComponentSpacePoseContext& Context) override;
+	virtual void EvaluateSkeletalControl_AnyThread(FComponentSpacePoseContext& Output, TArray<FBoneTransform>& OutBoneTransforms) override;
+	virtual bool IsValidToEvaluate(const USkeleton* Skeleton, const FBoneContainer& RequiredBones) override;
+	// End of FAnimNode_SkeletalControlBase interface
+
+private:
+	// FAnimNode_SkeletalControlBase interface
+	virtual void InitializeBoneReferences(const FBoneContainer& RequiredBones) override;
+	// End of FAnimNode_SkeletalControlBase interface
 };
