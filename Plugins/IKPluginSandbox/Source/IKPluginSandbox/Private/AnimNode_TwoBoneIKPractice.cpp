949a676d8790daa79e17dd16aa46baf01499585d
monguri <mongry@gmail.com>
2018-05-04 19:19:05 +0900

    SolveTwoBoneIK部分の写経実装をしてAnimNode_TwoBoneIKPraciticeの完成

--- Plugins/IKPluginSandbox/Source/IKPluginSandbox/Private/AnimNode_TwoBoneIKPractice.cpp
+++ Plugins/IKPluginSandbox/Source/IKPluginSandbox/Private/AnimNode_TwoBoneIKPractice.cpp
@@ -3,7 +3,6 @@
 #include "AnimNode_TwoBoneIKPractice.h"
 #include "AnimationRuntime.h"
 #include "Animation/AnimInstanceProxy.h"
-#include "TwoBoneIK.h" //TODO:後で消す
 
 FAnimNode_TwoBoneIKPractice::FAnimNode_TwoBoneIKPractice()
 	: EffectorLocationSpace(BCS_ComponentSpace)
@@ -34,6 +33,164 @@ FTransform FAnimNode_TwoBoneIKPractice::GetTargetTransform(const FTransform& InC
 	}
 
 	return OutTransform;
+}
+
+namespace
+{
+void SolveTwoBoneIK(const FVector& RootPos, const FVector& JointPos, const FVector& EndPos, const FVector& JointTarget, const FVector& Effector, FVector& OutJointPos, FVector& OutEndPos, float UpperLimbLength, float LowerLimbLength)
+{
+	// This is our reach goal.
+	FVector DesiredPos = Effector;
+	FVector DesiredDelta = DesiredPos - RootPos;
+	float DesiredLength = DesiredDelta.Size();
+
+	// Find lengths of upper and lower limb in the ref skeleton.
+	// Use actual sizes instead of ref skeleton, so we take into account translation and scaling from other bone controllers.
+	float MaxLimbLength = LowerLimbLength + UpperLimbLength;
+
+	// Check to handle case where DesiredPos is the same as RootPos.
+	FVector DesiredDir;
+	if (DesiredLength < (float)KINDA_SMALL_NUMBER)
+	{
+		DesiredLength = (float)KINDA_SMALL_NUMBER;
+		DesiredDir = FVector(1, 0, 0);
+	}
+	else
+	{
+		DesiredDir = DesiredDelta.GetSafeNormal();
+	}
+
+	// Get joint target (used for defining plane that joint should be in).
+	FVector JointTargetDelta = JointTarget - RootPos;
+	const float JointTargetLengthSqr = JointTargetDelta.SizeSquared();
+
+	// Same check as above, to cover case when JointTarget position is the same as RootPos.
+	FVector JointPlaneNormal, JointBendDir;
+	// JointBendDirと言ってるが、OutJointが実際に曲がる方向ではなく、JointPlaneに対して平行で、DesiredDirに対して垂直な軸にしたい
+	if (JointTargetLengthSqr < FMath::Square((float)KINDA_SMALL_NUMBER))
+	{
+		JointBendDir = FVector(0, 1, 0);
+		JointPlaneNormal = FVector(0, 0, 1);
+	}
+	else
+	{
+		JointPlaneNormal = DesiredDir ^ JointTargetDelta;
+
+		// If we are trying to point the limb in the same direction that we are supposed to displace the joint in, 
+		// we have to just pick 2 random vector perp to DesiredDir and each other.
+		if (JointPlaneNormal.SizeSquared() < FMath::Square((float)KINDA_SMALL_NUMBER))
+		{
+			DesiredDir.FindBestAxisVectors(JointPlaneNormal, JointBendDir);
+		}
+		else
+		{
+			JointPlaneNormal.Normalize();
+
+			// Find the final member of the reference frame by removing any component of JointTargetDelta along DesiredDir.
+			// This should never leave a zero vector, because we've checked DesiredDir and JointTargetDelta are not parallel.
+			JointBendDir = JointTargetDelta - (JointTargetDelta | DesiredDir) * DesiredDir;
+			JointBendDir.Normalize();
+		}
+	}
+
+	OutEndPos = DesiredPos;
+	// ここからやっとIK計算。TwoBoneIKならOutJointPosを解析的に解ける
+	OutJointPos = JointPos;
+
+	// If we are trying to reach a goal beyond the length of the limb, clamp it to something solvable and extend limb fully.
+	if (DesiredLength > MaxLimbLength)
+	{
+		// OutEndPosはそもそも初期拘束条件のDesiredPosに決まりなのだが、長さ的に無理なときは最高に関節を指定方向にまっすぐに伸ばした形にする
+		OutEndPos = RootPos + MaxLimbLength * DesiredDir;
+		OutJointPos = RootPos + UpperLimbLength * DesiredDir;
+	}
+	else
+	{
+		// ここは完全に三角関数の計算によってOutJointPosを求める部分である
+
+		// 余弦定理でTwoBoneのなす三角形のRoot部分の角度を求める
+		// So we have a triangle we know the side lengths of. We can work out the angle between DesiredDir and the direction of the upper limb
+		// using the sin rule:
+		const float TwoAB = 2.0f * UpperLimbLength * DesiredLength;
+
+		const float CosAngle = ((UpperLimbLength * UpperLimbLength) + (DesiredLength * DesiredLength) - (LowerLimbLength * LowerLimbLength)) / TwoAB;
+
+		// If CosAngle is less than 0, the upper arm actually points the opposite way to DesiredDir, so we handle that.
+		const bool bReverseUpperBone = (CosAngle < 0.0f);
+
+		// Angle between upper limb and DesiredDir
+		// ACos clamps internally so we dont need to worry about out-of-range values here.
+		const float Angle = FMath::Acos(CosAngle);
+
+		// Now we calculate the distance of the joint from the root -> effector line.
+		// This forms a right-angle triangle, with the upper limb as the hypotenuse.
+		const float JointLineDist = UpperLimbLength * FMath::Sin(Angle);
+
+		// And the final side of that triangle - distance along DesiredDir of perpendicular.
+		// ProjJointDistSqr can't be neg, because JointLineDist must be <= UpperLimbLength because appSin(Angle) is <= 1.
+		// ピタゴラスの定理
+		const float ProjJointDistSqr = (UpperLimbLength * UpperLimbLength) - (JointLineDist * JointLineDist);
+		float ProjJointDist = (ProjJointDistSqr > 0.0f) ? FMath::Sqrt(ProjJointDistSqr) : 0.0f;
+		if (bReverseUpperBone)
+		{
+			ProjJointDist *= -1.0f; // これは何気に必要性に気づきにくいよな。ベクトル計算と三角関数計算の違いを埋めるもの
+		}
+
+		// So now we can work out where to put the joint!
+		OutJointPos = RootPos + (ProjJointDist * DesiredDir) + (JointLineDist * JointBendDir);
+	}
+}
+
+void SolveTwoBoneIK(FTransform& InOutRootTransform, FTransform& InOutJointTransform, FTransform& InOutEndTransform, const FVector& JointTarget, const FVector& Effector, float UpperLimbLength, float LowerLimbLength)
+{
+	FVector OutJointPos, OutEndPos;
+
+	FVector RootPos = InOutRootTransform.GetLocation();
+	FVector JointPos = InOutJointTransform.GetLocation();
+	FVector EndPos = InOutEndTransform.GetLocation();
+
+	SolveTwoBoneIK(RootPos, JointPos, EndPos, JointTarget, Effector, OutJointPos, OutEndPos, UpperLimbLength, LowerLimbLength);
+
+	// Update transform for upper bone.
+	{
+		// Get difference in direction for old and new joint orientations
+		FVector const OldDir = (JointPos - RootPos).GetSafeNormal();
+		FVector const NewDir = (OutJointPos - RootPos).GetSafeNormal();
+		// Find Delta Rotation take takes us from Old to New dir
+		FQuat const DeltaRotation = FQuat::FindBetweenNormals(OldDir, NewDir);
+		// Rotate our Joint quaternion by this delta rotation
+		InOutRootTransform.SetRotation(DeltaRotation * InOutRootTransform.GetRotation());
+		// And put joint where it should be.
+		InOutRootTransform.SetTranslation(RootPos);
+	}
+
+	// update transform for middle bone
+	{
+		// Get difference in direction for old and new joint orientations
+		FVector const OldDir = (EndPos - JointPos).GetSafeNormal();
+		FVector const NewDir = (OutEndPos - OutJointPos).GetSafeNormal();
+		// Find Delta Rotation take takes us from Old to New dir
+		FQuat const DeltaRotation = FQuat::FindBetweenNormals(OldDir, NewDir);
+		// Rotate our Joint quaternion by this delta rotation
+		InOutJointTransform.SetRotation(DeltaRotation * InOutJointTransform.GetRotation());
+		// And put joint where it should be.
+		InOutJointTransform.SetTranslation(OutJointPos);
+	}
+
+	// Update transform for end bone.
+	// currently not doing anything to rotation
+	// keeping input rotation
+	// Set correct location for end bone.
+	InOutEndTransform.SetTranslation(OutEndPos);
+	// TODO:LocationとTranslationは同じなのになぜGetはLocationでSetはTranslationを使うんだろう？
+}
+
+void SolveTwoBoneIK(FTransform& InOutRootTransform, FTransform& InOutJointTransform, FTransform& InOutEndTransform, const FVector& JointTarget, const FVector& Effector)
+{
+	float LowerLimbLength = (InOutEndTransform.GetLocation() - InOutJointTransform.GetLocation()).Size();
+	float UpperLimbLength = (InOutJointTransform.GetLocation() - InOutRootTransform.GetLocation()).Size();
+	SolveTwoBoneIK(InOutRootTransform, InOutJointTransform, InOutEndTransform, JointTarget, Effector, UpperLimbLength, LowerLimbLength);
+}
 }
 
 void FAnimNode_TwoBoneIKPractice::EvaluateSkeletalControl_AnyThread(FComponentSpacePoseContext& Output, TArray<FBoneTransform>& OutBoneTransforms)
@@ -87,7 +244,7 @@ void FAnimNode_TwoBoneIKPractice::EvaluateSkeletalControl_AnyThread(FComponentSp
 	EndBoneCSTransform.SetLocation(InitialEndPos);
 #endif
 
-	AnimationCore::SolveTwoBoneIK(UpperLimbCSTransform, LowerLimbCSTransform, EndBoneCSTransform, JointTargetPosition, EffectorTargetPosition, 0, 0, 0);
+	SolveTwoBoneIK(UpperLimbCSTransform, LowerLimbCSTransform, EndBoneCSTransform, JointTargetPosition, EffectorTargetPosition);
 
 	// Update transform for upper bone.
 	{
