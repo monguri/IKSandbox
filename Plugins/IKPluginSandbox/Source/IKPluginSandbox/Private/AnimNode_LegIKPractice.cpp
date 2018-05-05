b4083724127b1b12b329f273c0a12e1cfd21709d
monguri <mongry@gmail.com>
2018-05-05 15:25:49 +0900

    AnimNode_LegIKPracticeで、いくつか最初から必要になるメソッドや変数のひながたを用意した

--- Plugins/IKPluginSandbox/Source/IKPluginSandbox/Private/AnimNode_LegIKPractice.cpp
+++ Plugins/IKPluginSandbox/Source/IKPluginSandbox/Private/AnimNode_LegIKPractice.cpp
@@ -2,3 +2,21 @@
 
 #include "AnimNode_LegIKPractice.h"
 
+FAnimNode_LegIKPractice::FAnimNode_LegIKPractice()
+{
+	ReachPrecision = 0.01f;
+	MaxIterations = 12;
+}
+
+void FAnimNode_LegIKPractice::EvaluateSkeletalControl_AnyThread(FComponentSpacePoseContext& Output, TArray<FBoneTransform>& OutBoneTransforms)
+{
+}
+
+bool FAnimNode_LegIKPractice::IsValidToEvaluate(const USkeleton* Skeleton, const FBoneContainer& RequiredBones)
+{
+	return true;
+}
+
+void FAnimNode_LegIKPractice::InitializeBoneReferences(const FBoneContainer& RequiredBones)
+{
+}
--- Plugins/IKPluginSandbox/Source/IKPluginSandbox/Public/AnimNode_LegIKPractice.h
+++ Plugins/IKPluginSandbox/Source/IKPluginSandbox/Public/AnimNode_LegIKPractice.h
@@ -7,11 +7,93 @@
 #include "BoneControllers/AnimNode_SkeletalControlBase.h"
 #include "AnimNode_LegIKPractice.generated.h"
 
-/**
- * 
- */
+/** Per foot definitions */
+USTRUCT()
+struct FAnimLegIKDefinitionPractice
+{
+	GENERATED_USTRUCT_BODY()
+
+	UPROPERTY(EditAnywhere, Category = "Settings")
+	FBoneReference IKFootBone;
+
+	UPROPERTY(EditAnywhere, Category = "Settings")
+	FBoneReference FKFootBone;
+
+	UPROPERTY(EditAnywhere, Category = "Settings")
+	int32 NumBonesInLimb;
+
+	/** Forward Axis for Foot bone. */
+	UPROPERTY(EditAnywhere, Category = "Settings")
+	TEnumAsByte<EAxis::Type> FootBoneForwardAxis;
+
+	/** If enabled, we prevent the leg from bending backwards and enforce a min compression angle */
+	UPROPERTY(EditAnywhere, Category = "Settings")
+	bool bEnableRotationLimit;
+
+	/** Only used if bEnableRotationLimit is enabled. Prevents the leg from folding onto itself,
+	* and forces at least this angle between Parent and Child bone. */
+	UPROPERTY(EditAnywhere, Category = "Settings")
+	float MinRotationAngle;
+
+	FAnimLegIKDefinitionPractice()
+		: NumBonesInLimb(2)
+		, FootBoneForwardAxis(EAxis::Y)
+		, bEnableRotationLimit(false)
+		, MinRotationAngle(15.f)
+	{}
+};
+
+/** Runtime foot data after validation, we guarantee these bones to exist */
+USTRUCT()
+struct FAnimLegIKDataPractice
+{
+	GENERATED_USTRUCT_BODY()
+	// TODO:後で必要になった時に必要になったものを定義する
+};
+
+USTRUCT()
+struct FIKChainLinkPractice
+{
+	GENERATED_USTRUCT_BODY()
+	// TODO:後で必要になった時に必要になったものを定義する
+};
+
+USTRUCT()
+struct FIKChainPractice
+{
+	GENERATED_USTRUCT_BODY()
+	// TODO:後で必要になった時に必要になったものを定義する
+};
+
 USTRUCT(BlueprintInternalUseOnly)
 struct IKPLUGINSANDBOX_API FAnimNode_LegIKPractice : public FAnimNode_SkeletalControlBase
 {
 	GENERATED_USTRUCT_BODY()
+
+	FAnimNode_LegIKPractice();
+
+	/** Tolerance for reaching IK Target, in unreal units. */
+	UPROPERTY(EditAnywhere, Category = "Settings")
+	float ReachPrecision;
+
+	/** Max Number of Iterations. */
+	UPROPERTY(EditAnywhere, Category = "Settings")
+	int32 MaxIterations;
+
+	UPROPERTY(EditAnywhere, Category = "Settings")
+	TArray<FAnimLegIKDefinitionPractice> LegsDefinition;
+
+	UPROPERTY(Transient)
+	TArray<FAnimLegIKDataPractice> LegsData;
+
+public:
+	// FAnimNode_SkeletalControlBase interface
+	virtual void EvaluateSkeletalControl_AnyThread(FComponentSpacePoseContext& Output, TArray<FBoneTransform>& OutBoneTransforms) override;
+	virtual bool IsValidToEvaluate(const USkeleton* Skeleton, const FBoneContainer& RequiredBones) override;
+	// End of FAnimNode_SkeletalControlBase interface
+
+private:
+	// FAnimNode_SkeletalControlBase interface
+	virtual void InitializeBoneReferences(const FBoneContainer& RequiredBones) override;
+	// End of FAnimNode_SkeletalControlBase interface
 };
