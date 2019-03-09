d43da5aa5d7cc1eec26cd3266f6159645a39be4d
monguri <mongry@gmail.com>
2019-03-09 18:27:43 +0900

    Localスペースでの扱いかどうかに関係ない部分を、ある程度、JacobianIKノードから持ってきた。保存のためサブミット

--- Plugins/IKPluginSandbox/Source/IKPluginSandbox/Private/AnimNode_JacobianIKLocal.cpp
+++ Plugins/IKPluginSandbox/Source/IKPluginSandbox/Private/AnimNode_JacobianIKLocal.cpp
@@ -3,7 +3,135 @@
 #include "AnimNode_JacobianIKLocal.h"
 #include "Animation/AnimInstanceProxy.h"
 
+FAnimNode_JacobianIKLocal::AnySizeMatrix::AnySizeMatrix()
+{
+}
+
+FAnimNode_JacobianIKLocal::AnySizeMatrix::AnySizeMatrix(uint8 _NumRow, uint8 _NumColumn)
+{
+	Elements.AddZeroed(_NumRow * _NumColumn);
+	NumRow = _NumRow;
+	NumColumn = _NumColumn;
+}
+
+float FAnimNode_JacobianIKLocal::AnySizeMatrix::Get(uint8 Row, uint8 Column) const
+{
+	return Elements[Row * NumColumn + Column];
+}
+
+void FAnimNode_JacobianIKLocal::AnySizeMatrix::Set(uint8 Row, uint8 Column, float Value)
+{
+	// TODO:operator[][]の演算子オーバーライドは作れないかな？
+	Elements[Row * NumColumn + Column] = Value;
+}
+
+void FAnimNode_JacobianIKLocal::AnySizeMatrix::ZeroClear()
+{
+	FMemory::Memzero(Elements.GetData(), sizeof(float) * NumRow * NumColumn);
+}
+
+void FAnimNode_JacobianIKLocal::AnySizeMatrix::Transpose(const AnySizeMatrix& InMatrix, AnySizeMatrix& OutMatrix)
+{
+	for (int32 Row = 0; Row < InMatrix.NumRow; ++Row)
+	{
+		for (int32 Column = 0; Column < InMatrix.NumColumn; ++Column)
+		{
+			OutMatrix.Set(Column, Row, InMatrix.Get(Row, Column));
+		}
+	}
+}
+
+void FAnimNode_JacobianIKLocal::AnySizeMatrix::Multiply(const AnySizeMatrix& A, const AnySizeMatrix& B, AnySizeMatrix& OutResult)
+{
+	check(A.NumColumn == B.NumRow);
+	check(OutResult.NumRow == A.NumRow);
+	check(OutResult.NumColumn == B.NumColumn);
+
+	for (int32 Row = 0; Row < OutResult.NumRow; ++Row)
+	{
+		for (int32 Column = 0; Column < OutResult.NumColumn; ++Column)
+		{
+			OutResult.Set(Row, Column, 0);
+
+			for (int32 i = 0; i < A.NumColumn; ++i)
+			{
+				OutResult.Set(Row, Column, OutResult.Get(Row, Column) + A.Get(Row, i) * B.Get(i, Column));
+			}
+		}
+	}
+}
+
+void FAnimNode_JacobianIKLocal::AnySizeMatrix::Add(const AnySizeMatrix& A, const AnySizeMatrix& B, AnySizeMatrix& OutResult)
+{
+	check(A.NumRow == B.NumRow);
+	check(A.NumColumn == B.NumColumn);
+	check(OutResult.NumRow == A.NumRow);
+	check(OutResult.NumColumn == B.NumColumn);
+
+	for (int32 Row = 0; Row < OutResult.NumRow; ++Row)
+	{
+		for (int32 Column = 0; Column < OutResult.NumColumn; ++Column)
+		{
+			OutResult.Set(Row, Column, A.Get(Row, Column) + B.Get(Row, Column));
+		}
+	}
+}
+
+float FAnimNode_JacobianIKLocal::AnySizeMatrix::Inverse3x3(const AnySizeMatrix& InMatrix, AnySizeMatrix& OutMatrix)
+{
+	float Determinant =
+		  InMatrix.Get(0, 0) * InMatrix.Get(1, 1) * InMatrix.Get(2, 2)
+		+ InMatrix.Get(1, 0) * InMatrix.Get(2, 1) * InMatrix.Get(0, 2)
+		+ InMatrix.Get(2, 0) * InMatrix.Get(0, 1) * InMatrix.Get(1, 2)
+		- InMatrix.Get(0, 0) * InMatrix.Get(2, 1) * InMatrix.Get(1, 2)
+		- InMatrix.Get(2, 0) * InMatrix.Get(1, 1) * InMatrix.Get(0, 2)
+		- InMatrix.Get(1, 0) * InMatrix.Get(0, 1) * InMatrix.Get(2, 2);
+
+	if (Determinant == 0)
+	{
+		return Determinant;
+	}
+
+	OutMatrix.Set(0, 0, (InMatrix.Get(1, 1) * InMatrix.Get(2, 2) - InMatrix.Get(1, 2) * InMatrix.Get(2, 1)) / Determinant);
+	OutMatrix.Set(0, 1, (InMatrix.Get(0, 2) * InMatrix.Get(2, 1) - InMatrix.Get(0, 1) * InMatrix.Get(2, 2)) / Determinant);
+	OutMatrix.Set(0, 2, (InMatrix.Get(0, 1) * InMatrix.Get(1, 2) - InMatrix.Get(0, 2) * InMatrix.Get(1, 1)) / Determinant);
+
+	OutMatrix.Set(1, 0, (InMatrix.Get(1, 2) * InMatrix.Get(2, 0) - InMatrix.Get(1, 0) * InMatrix.Get(2, 2)) / Determinant);
+	OutMatrix.Set(1, 1, (InMatrix.Get(0, 0) * InMatrix.Get(2, 2) - InMatrix.Get(0, 2) * InMatrix.Get(2, 0)) / Determinant);
+	OutMatrix.Set(1, 2, (InMatrix.Get(0, 2) * InMatrix.Get(1, 0) - InMatrix.Get(0, 0) * InMatrix.Get(1, 2)) / Determinant);
+
+	OutMatrix.Set(2, 0, (InMatrix.Get(1, 0) * InMatrix.Get(2, 1) - InMatrix.Get(1, 1) * InMatrix.Get(2, 0)) / Determinant);
+	OutMatrix.Set(2, 1, (InMatrix.Get(0, 1) * InMatrix.Get(2, 0) - InMatrix.Get(0, 0) * InMatrix.Get(2, 1)) / Determinant);
+	OutMatrix.Set(2, 2, (InMatrix.Get(0, 0) * InMatrix.Get(1, 1) - InMatrix.Get(0, 1) * InMatrix.Get(1, 0)) / Determinant);
+
+	return Determinant;
+}
+
+void FAnimNode_JacobianIKLocal::AnySizeMatrix::TransformVector(const AnySizeMatrix& InMatrix, const TArray<float>& InVector, TArray<float>& OutVector)
+{
+	check((int32)InMatrix.NumRow == InVector.Num());
+	check((int32)InMatrix.NumColumn == OutVector.Num());
+
+	for (int32 Column = 0; Column < OutVector.Num(); ++Column)
+	{
+		OutVector[Column] = 0.0f;
+	}
+
+	for (int32 Column = 0; Column < InMatrix.NumColumn; ++Column)
+	{
+		for (int32 Row = 0; Row < InMatrix.NumRow; ++Row)
+		{
+			OutVector[Column] += InVector[Row] * InMatrix.Get(Row, Column);
+		}
+	}
+}
+
 FAnimNode_JacobianIKLocal::FAnimNode_JacobianIKLocal()
+	: EffectorTargetLocation(0.0f, 0.0f, 0.0f)
+	, NumIteration(10)
+	, Precision(SMALL_NUMBER)
+	, IKRootJointParent(INDEX_NONE)
+	, Lambda(0.0f)
 {
 }
 
--- Plugins/IKPluginSandbox/Source/IKPluginSandbox/Public/AnimNode_JacobianIKLocal.h
+++ Plugins/IKPluginSandbox/Source/IKPluginSandbox/Public/AnimNode_JacobianIKLocal.h
@@ -5,6 +5,7 @@
 #include "CoreMinimal.h"
 #include "UObject/NoExportTypes.h"
 #include "AnimNode_SkeletalControlBaseLocal.h"
+#include "BoneIndices.h"
 #include "AnimNode_JacobianIKLocal.generated.h"
 
 USTRUCT(BlueprintInternalUseOnly)
@@ -12,9 +13,76 @@ struct IKPLUGINSANDBOX_API FAnimNode_JacobianIKLocal : public FAnimNode_Skeletal
 {
 	GENERATED_USTRUCT_BODY()
 
+	/** Name of ik start joint. **/
+	UPROPERTY(EditAnywhere, Category=IK)
+	FBoneReference IKRootJoint;
+
+	/** Name of ik effector joint. **/
+	UPROPERTY(EditAnywhere, Category=IK)
+	FBoneReference EffectorJoint;
+
+	/** Effector Target Location. Component space. **/
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=IK, meta=(PinShownByDefault))
+	FVector EffectorTargetLocation;
+
+	/** The number of iteration. **/
+	UPROPERTY(EditAnywhere, Category=IK)
+	uint32 NumIteration;
+
+	/** Tolerance for IK Target and IK joint length, in unreal units. */
+	UPROPERTY(EditAnywhere, Category=IK)
+	float Precision;
+
+	/** Normalize parameter for singular pose. */
+	UPROPERTY(EditAnywhere, Category=IK)
+	float Lambda;
+
 	FAnimNode_JacobianIKLocal();
 
-public:
+private:
+	struct IKJointWorkData
+	{
+		FCompactPoseBoneIndex BoneIndex;
+		FTransform ComponentTransform;
+		FTransform LocalTransform;
+
+		IKJointWorkData() : BoneIndex(INDEX_NONE), ComponentTransform(FTransform::Identity), LocalTransform(FTransform::Identity) {}
+		IKJointWorkData(FCompactPoseBoneIndex _BoneIndex, const FTransform& _ComponentTransform, const FTransform& _LocalTransform) : BoneIndex(_BoneIndex), ComponentTransform(_ComponentTransform), LocalTransform(_LocalTransform) {}
+	};
+
+	TArray<IKJointWorkData> IKJointWorkDatas;
+
+	struct AnySizeMatrix
+	{
+		AnySizeMatrix();
+		AnySizeMatrix(uint8 _NumRow, uint8 _NumColumn);
+		float Get(uint8 Row, uint8 Column) const;
+		void Set(uint8 Row, uint8 Column, float Value);
+		void ZeroClear();
+		static void Transpose(const AnySizeMatrix& InMatrix, AnySizeMatrix& OutMatrix);
+		static void Multiply(const AnySizeMatrix& A, const AnySizeMatrix& B, AnySizeMatrix& OutResult);
+		static void Add(const AnySizeMatrix& A, const AnySizeMatrix& B, AnySizeMatrix& OutResult);
+		static float Inverse3x3(const AnySizeMatrix& InMatrix, AnySizeMatrix& OutMatrix);
+		static void TransformVector(const AnySizeMatrix& InMatrix, const TArray<float>& InVector, TArray<float>& OutVector);
+
+		TArray<float> Elements; // 1-dimensional array for access speed.
+		uint8 NumRow;
+		uint8 NumColumn;
+	};
+
+	// Parent joint of ik root joint
+	FCompactPoseBoneIndex IKRootJointParent;
+
+	AnySizeMatrix Jacobian;
+	AnySizeMatrix Jt;
+	AnySizeMatrix JJt;
+	AnySizeMatrix JJtPlusLambdaI;
+	AnySizeMatrix JJti;
+	AnySizeMatrix PseudoInverseJacobian;
+	AnySizeMatrix LambdaI;
+	TArray<float> IterationStepPosition;
+	TArray<float> IterationStepAngles;
+
 	// FAnimNode_SkeletalControlBaseLocal interface
 	virtual void InitializeBoneReferences(const FBoneContainer& RequiredBones) override;
 	virtual bool IsValidToEvaluate(const USkeleton* Skeleton, const FBoneContainer& RequiredBones) override;
