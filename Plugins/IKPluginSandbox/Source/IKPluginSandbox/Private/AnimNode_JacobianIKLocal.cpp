68dadae7e6e68ab3dd9ca8112de87b9fdf0d1e0a
monguri <mongry@gmail.com>
2019-03-09 19:15:49 +0900

    Evaluate以外の処理をJacobianIKノードからコピーしてきた

--- Plugins/IKPluginSandbox/Source/IKPluginSandbox/Private/AnimNode_JacobianIKLocal.cpp
+++ Plugins/IKPluginSandbox/Source/IKPluginSandbox/Private/AnimNode_JacobianIKLocal.cpp
@@ -3,6 +3,81 @@
 #include "AnimNode_JacobianIKLocal.h"
 #include "Animation/AnimInstanceProxy.h"
 
+namespace
+{
+// XYZの3成分に使う定数
+const int32 AXIS_COUNT = 3;
+// 回転方向の3成分に使う定数
+const int32 ROTATION_AXIS_COUNT = 3;
+
+// degree単位でのsin/cos関数
+float SinD(float degree) { return FMath::Sin(FMath::DegreesToRadians(degree)); }
+float CosD(float degree) { return FMath::Cos(FMath::DegreesToRadians(degree)); }
+
+// 各軸の回転行列
+//TODO: 正負の符号や、行優先列優先についてはあまりきちんと考慮はしておらずほげたつさんのソースを参考にしている
+FMatrix RotationX(float Roll)
+{
+	return FMatrix(
+		FPlane(1, 0, 0, 0),
+		FPlane(0, CosD(Roll), -SinD(Roll), 0),
+		FPlane(0, SinD(Roll), CosD(Roll), 0),
+		FPlane(0, 0, 0, 1)
+	);
+}
+
+FMatrix RotationY(float Pitch)
+{
+	return FMatrix(
+		FPlane(CosD(Pitch), 0, SinD(Pitch), 0),
+		FPlane(0, 1, 0, 0),
+		FPlane(-SinD(Pitch), 0, CosD(Pitch), 0),
+		FPlane(0, 0, 0, 1)
+	);
+}
+
+FMatrix RotationZ(float Yaw)
+{
+	return FMatrix(
+		FPlane(CosD(Yaw), SinD(Yaw), 0, 0),
+		FPlane(-SinD(Yaw), CosD(Yaw), 0, 0),
+		FPlane(0, 0, 1, 0),
+		FPlane(0, 0, 0, 1)
+	);
+}
+
+// 各軸の回転行列を回転角の変数で微分した行列
+FMatrix RotationDifferentialX(float Roll)
+{
+	return FMatrix(
+		FPlane(0, 0, 0, 0),
+		FPlane(0, -SinD(Roll), -CosD(Roll), 0),
+		FPlane(0, CosD(Roll), -SinD(Roll), 0),
+		FPlane(0, 0, 0, 0)
+	);
+}
+
+FMatrix RotationDifferentialY(float Pitch)
+{
+	return FMatrix(
+		FPlane(-SinD(Pitch), 0, CosD(Pitch), 0),
+		FPlane(0, 0, 0, 0),
+		FPlane(-CosD(Pitch), 0, -SinD(Pitch), 0),
+		FPlane(0, 0, 0, 0)
+	);
+}
+
+FMatrix RotationDifferentialZ(float Yaw)
+{
+	return FMatrix(
+		FPlane(-SinD(Yaw), CosD(Yaw), 0, 0),
+		FPlane(-CosD(Yaw), -SinD(Yaw), 0, 0),
+		FPlane(0, 0, 0, 0),
+		FPlane(0, 0, 0, 0)
+	);
+}
+} // namespace
+
 FAnimNode_JacobianIKLocal::AnySizeMatrix::AnySizeMatrix()
 {
 }
@@ -137,11 +212,80 @@ FAnimNode_JacobianIKLocal::FAnimNode_JacobianIKLocal()
 
 void FAnimNode_JacobianIKLocal::InitializeBoneReferences(const FBoneContainer& RequiredBones)
 {
+	bool Success = IKRootJoint.Initialize(RequiredBones);
+	if (!Success)
+	{
+		return;
+	}
+
+	EffectorJoint.Initialize(RequiredBones);
+	if (!Success)
+	{
+		return;
+	}
+
+	IKRootJointParent = FCompactPoseBoneIndex(RequiredBones.GetParentBoneIndex(IKRootJoint.BoneIndex));
+
+	// EffectorJointから、IKRootJointまで、IKRootJointにぶつからなければルートジョイントまでワークデータを持たせる
+	FCompactPoseBoneIndex IKJointIndex = EffectorJoint.GetCompactPoseIndex(RequiredBones);
+
+	IKJointWorkDatas.Reset(IKJointWorkDatas.Num());
+
+	while (IKJointIndex != INDEX_NONE && IKJointIndex != IKRootJoint.BoneIndex)
+	{
+		IKJointWorkDatas.Emplace(IKJointIndex, FTransform::Identity, FTransform::Identity);
+
+		IKJointIndex = RequiredBones.GetParentBoneIndex(IKJointIndex);
+	}
+
+	if (IKJointIndex == IKRootJoint.BoneIndex)
+	{
+		IKJointWorkDatas.Emplace(IKJointIndex, FTransform::Identity, FTransform::Identity);
+	}
+
+	Jacobian = AnySizeMatrix((IKJointWorkDatas.Num() - 1) * ROTATION_AXIS_COUNT, AXIS_COUNT);
+	Jt = AnySizeMatrix(AXIS_COUNT, (IKJointWorkDatas.Num() - 1) * ROTATION_AXIS_COUNT);
+	JJt = AnySizeMatrix(AXIS_COUNT, AXIS_COUNT); // J * Jt
+
+	LambdaI = AnySizeMatrix(AXIS_COUNT, AXIS_COUNT); // lambda * I
+	LambdaI.ZeroClear();
+	LambdaI.Set(0, 0, Lambda);
+	LambdaI.Set(1, 1, Lambda);
+	LambdaI.Set(2, 2, Lambda);
+
+	JJtPlusLambdaI = AnySizeMatrix(AXIS_COUNT, AXIS_COUNT); // J * J^t + lambda * I
+	JJti = AnySizeMatrix(AXIS_COUNT, AXIS_COUNT); // (J * J^t + lambda * I)^-1
+	PseudoInverseJacobian = AnySizeMatrix(AXIS_COUNT, (IKJointWorkDatas.Num() - 1) * ROTATION_AXIS_COUNT); // J^t * (J * J^t)^-1
+
+	IterationStepPosition.SetNumZeroed(AXIS_COUNT);
+	IterationStepAngles.SetNumZeroed((IKJointWorkDatas.Num() - 1) * ROTATION_AXIS_COUNT);
 }
 
 bool FAnimNode_JacobianIKLocal::IsValidToEvaluate(const USkeleton* Skeleton, const FBoneContainer& RequiredBones)
 {
-	return true;
+	if (!IKRootJoint.IsValidToEvaluate(RequiredBones))
+	{
+		return false;
+	}
+
+	if (!EffectorJoint.IsValidToEvaluate(RequiredBones))
+	{
+		return false;
+	}
+
+	FCompactPoseBoneIndex ParentIndex = EffectorJoint.GetCompactPoseIndex(RequiredBones);
+	while (ParentIndex != INDEX_NONE && ParentIndex != IKRootJoint.BoneIndex)
+	{
+		ParentIndex = RequiredBones.GetParentBoneIndex(ParentIndex);
+	}
+
+	if (ParentIndex == INDEX_NONE)
+	{
+		// IKRootJointとEffectorJointが先祖子孫関係になってない
+		return false;
+	}
+
+	return (IKJointWorkDatas.Num() >= 2); // 最低限エフェクタと、一つIK制御ジョイントがないと意味がない
 }
 
 void FAnimNode_JacobianIKLocal::EvaluateSkeletalControl_AnyThread(const FCSPose<FCompactPose>& InPose, FPoseContext& Output)
